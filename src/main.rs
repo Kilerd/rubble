1da5c49dfb4b2a14508991a875fbe1b5e8587f74
Kilerd Chan <blove694@gmail.com>
2018-09-14 00:35:12 +0800

    feat: migration and project structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 extern crate rocket;
+extern crate diesel;
+extern crate dotenv;
+extern crate r2d2;
+
+#[macro_use]
+extern crate rocket_contrib;
+
+#[macro_use]
+extern crate serde_derive;
+
+mod pg_pool;
+mod schema;
+use dotenv::dotenv;
+
 
 #[get("/")]
 fn index() -> String {
@@ -8,5 +22,11 @@ fn index() -> String {
 }
 
 fn main() {
-    rocket::ignite().mount("/",routes![index]).launch();
+    dotenv().ok();
+    let database_url = std::env::var("database_url").expect("database_url must be set");
+
+    rocket::ignite()
+        .manage(pg_pool::init(&database_url))
+        .mount("/",routes![index])
+        .launch();
 }
