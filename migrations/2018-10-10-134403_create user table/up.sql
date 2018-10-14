d9d9484c0b6970cbcb77e47369457dd6533a5e0c
Kilerd Chan <blove694@gmail.com>
2018-10-14 10:23:26 +0800

    fix: using encrypted password in database migration file.

--- migrations/2018-10-10-134403_create user table/up.sql
+++ migrations/2018-10-10-134403_create user table/up.sql
@@ -8,4 +8,4 @@ CREATE TABLE users (
   PRIMARY KEY ("id")
 );
 
-INSERT INTO "public"."users"("id", "username", "password", "create_at", "last_login_at") VALUES(1, 'admin', 'admin', '2018-10-10 14:01:35', '2018-10-10 14:01:35');
+INSERT INTO "public"."users"("id", "username", "password", "create_at", "last_login_at") VALUES(1, 'admin', 'c0067d4af4e87f00dbac63b6156828237059172d1bbeac67427345d6a9fda484', '2018-10-10 14:01:35', '2018-10-10 14:01:35');
