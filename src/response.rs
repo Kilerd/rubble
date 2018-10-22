32addc941703255bb8d08e82ba8ea4cc64f4bef4
Kilerd Chan <blove694@gmail.com>
2018-10-22 15:09:56 +0800

    feat: support description

--- src/response.rs
+++ src/response.rs
@@ -8,20 +8,24 @@ pub struct PostResponse<'a> {
     pub post: &'a Post,
     pub timestamp: i64,
     pub markdown_content: String,
+    pub description: String,
 }
 
 impl<'a> PostResponse<'a> {
 
     pub fn from(post: &'a Post) -> PostResponse {
+        let content_split: Vec<_> = post.body.split("<!--more-->").collect();
+        let description_parser = Parser::new(&content_split[0]);
         let parser = Parser::new(&post.body);
-
+        let mut description_buf = String::new();
         let mut content_buf = String::new();
         html::push_html(&mut content_buf, parser);
-
+        html::push_html(&mut description_buf, description_parser);
         PostResponse {
             post,
             timestamp: post.publish_at.timestamp(),
-            markdown_content: content_buf
+            markdown_content: content_buf,
+            description: description_buf,
         }
     }
 }
