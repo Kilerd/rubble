3497e98ffd91db0b1db9c4723d5f4de151152ddf
Kilerd Chan <blove694@gmail.com>
2019-06-03 17:11:31 +0800

    feat: panel prototype

---
+++ panel/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+
+export default class App extends React.Component {
+    render() {
+        return( 
+            <div>
+                hello world
+            </div>
+        )
+    }
+}
---
+++ panel/index.tsx
@@ -0,0 +1,10 @@
+// index.tsx
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+import App from './components/App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+)
